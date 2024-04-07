cda1690bf15014e9b5abc9d104cc8a75e686ad79
Amanda Kaori <125595801+projamandakaori@users.noreply.github.com>
2024-04-07 20:55:32 -0300

    19/02

---
+++ aula01/code.js
@@ -0,0 +1,20 @@
+function csoma(){
+    let n1 = document.getElementById("num1").value
+    let n2 = document.getElementById("num2").value
+
+    const total = n1 + n2
+    alert("A soma é: " + total)
+}
+
+
+function cnome(){
+    let n = document.getElementById("nome").value
+    alert("Seu nome é: " + n)
+}
+
+function ola(){
+    console.log("Olá JS")
+    alert("Vc viu o JS no navegador!")
+}
+
+
